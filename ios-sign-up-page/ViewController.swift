5ce83c3b4a202c346eb2235716af8d01058aa177
Sahil Garg <sahilgarg@Sahils-Laptop.local>
2024-05-18 19:12:54 +0530

    setup-xCode-project

---
+++ ios-sign-up-page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-sign-up-page
+//
+//  Created by Sahil Garg on 18/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-sign-up-pageUITests/ios_sign_up_pageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_sign_up_pageUITestsLaunchTests.swift
+//  ios-sign-up-pageUITests
+//
+//  Created by Sahil Garg on 18/05/24.
+//
+
+import XCTest
+
+final class ios_sign_up_pageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
